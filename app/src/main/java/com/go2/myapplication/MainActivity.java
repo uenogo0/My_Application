dc15a4f6aed6be817525e29734d456a88296bc49
ueno <gou5ueno5@gmail.com>
2021-06-10 21:36:23 +0900

    test

--- app/src/main/java/com/go2/myapplication/MainActivity.java
+++ app/src/main/java/com/go2/myapplication/MainActivity.java
@@ -3,7 +3,7 @@ package com.go2.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//tesuto
 public class MainActivity extends AppCompatActivity {
 
     @Override
